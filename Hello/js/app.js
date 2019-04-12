f7d730e07e2147d7954b9d05644ba7706bac7b72
JDode <2420498526@qq.com>
2019-04-12 15:43:02 +0800

    add update

--- Hello/js/app.js
+++ Hello/js/app.js
@@ -6,7 +6,7 @@ document.write("</p>");
 document.write('<div id="msg">正在测试网络延迟，请稍后...</div>');
 document.write('<a href="#">电信网路</a>    <span class="classtime" xl-name="电信网路"></span><br>');
 document.write('<a href="#">联通网路</a>    <span class="classtime" xl-name="联通网路"></span>');
-var jump = 1, t = {}, autourl = new Array(), autoname = [];
+var t = {}, autourl = new Array(), autoname = [];
 autourl[1] = "http://image.baidu.com/"; //这个是电信服务器站点
 autourl[2] = "https://www.baidu.com/"; //这个是联通服务器站点
 autoname[1] = "电信网路";
@@ -19,14 +19,7 @@ autoname[2] = "联通网路";
             return function () {
                 t[autourl[j]] = (new Date()) - t[autourl[j]];  //记入时间差
                 console.log(autourl[j] + "    ：" + t[autourl[j]] + "ms"); //console.log(t[url] + "ms");
-                document.querySelector('[xl-name="' + autoname[j] + '"]').innerHTML = t[autourl[j]] + ' ms';
-                console.log(jump);
-                if (jump) {
-                    jump = 0;
-                    document.getElementById("msg").innerText = '3秒后进入【' + autoname[j] + '】';
-                    //setTimeout(function(){top.location=url;},3000); //setTimeout("top.location='" + url + "';",3000);   //3s 即3000ms
-                    //setTimeout(function () { window.location.replace(autourl[j]); }, 3000);
-                }
+                console.log(t[autourl[j]] + ' ms');
             }
         })(i);
         //闭包传值
